e891a25a8f9ddeb4913a0e7528bcefeaf01778d4
thaaoblues <thaaoblues81@gmail.com>
2025-06-02 15:56:37 +0200

    feat : EDSL

--- MicroFP.hs
+++ MicroFP.hs
@@ -15,6 +15,50 @@ import Test.QuickCheck.All
 
 
 
+-- FP3.1 
+
+
+-- ⟨program⟩::=( ⟨function⟩ )+
+-- ⟨function⟩::=identifier (identifier | integer) ∗ ’:=’ ⟨expr⟩ ’;’
+-- ⟨expr⟩::=⟨term⟩ | ⟨term⟩ ( ’+’ | ’-’ ) ⟨expr⟩
+-- ⟨term⟩::=⟨factor⟩ | ⟨factor⟩ ’*’ ⟨term⟩
+-- ⟨factor⟩::=integer
+-- ⟨ordering⟩
+-- |identifier ( ’(’ ⟨expr⟩ (’,’⟨expr⟩) ∗ ’)’)?
+-- |’if’ ’(’ ⟨expr⟩ ⟨ordering⟩ ⟨expr⟩ ’)’ ’then’ ’{’ ⟨expr⟩ ’}’ ’else’ ’{’ ⟨expr⟩ ’}’
+-- |’(’ ⟨expr⟩ ’)’
+-- ::=
+-- ’<’ | ’==’ | ’>’
+
+data Prog = Program [Procedure]
+            deriving Show
+
+data Procedure = Procedure Expr [Param] Expr
+            deriving Show
+
+data Identifier = Identifier Expr
+            deriving Show
+
+-- | Represents a parameter which can be an identifier or an integer
+data Param = IdParam Expr | IntParam Expr
+  deriving Show
+
+data Expr = Const Integer
+          | Var String
+          | Mult Expr Expr
+          | Add Expr Expr
+          | Sub Expr Expr
+          | If Condition Expr Expr
+          | Then Expr
+          | Else Expr
+          | Dec Expr
+          | Call String Expr
+          deriving Show
+
+data Condition = Cond Comparator Expr Expr
+                deriving Show
+
+data Comparator = Eq | Lt | Gt deriving Show
 
 
 -- QuickCheck: all prop_* tests
