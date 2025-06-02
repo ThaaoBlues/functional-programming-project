0dead0a95dc0258a04db1dd14bc5c90478db4530
thaaoblues <thaaoblues81@gmail.com>
2025-06-02 16:10:47 +0200

    feat : Pretty printing of EDSL

--- MicroFP.hs
+++ MicroFP.hs
@@ -43,7 +43,7 @@ data Identifier = Identifier Expr
 data Param = IdParam Expr | IntParam Expr
   deriving Show
 
-data Expr = Const Integer
+data Expr = IntConst Integer
           | Var String
           | Mult Expr Expr
           | Add Expr Expr
@@ -60,6 +60,52 @@ data Condition = Cond Comparator Expr Expr
 
 data Comparator = Eq | Lt | Gt deriving Show
 
+
+
+-- Pretty-printing
+
+
+
+pretty :: Prog -> String
+pretty (Program procedures) = foldr (\x s-> x ++ "\n"++ s) "" $ map prettyProcedure procedures
+-- the foldr is to build a global string with all precedures separated by newlines
+
+prettyProcedure :: Procedure -> String
+prettyProcedure (Procedure name params expr) =
+  prettyExpr name ++ " " ++ foldr (\x s-> x ++ " "++ s) "" (map prettyParam params) ++ " := " ++ prettyExpr expr ++ ";"
+-- the foldr is to build a global string with all parameters separated by spaces
+
+prettyParam :: Param -> String
+prettyParam (IdParam expr) = prettyExpr expr
+prettyParam (IntParam expr) = prettyExpr expr
+
+prettyExpr :: Expr -> String
+prettyExpr (IntConst i) = show i
+prettyExpr (Var s) = s
+prettyExpr (Mult e1 e2) = "(" ++ prettyExpr e1 ++ " * " ++ prettyExpr e2 ++ ")"
+prettyExpr (Add e1 e2) = "(" ++ prettyExpr e1 ++ " + " ++ prettyExpr e2 ++ ")"
+prettyExpr (Sub e1 e2) = "(" ++ prettyExpr e1 ++ " - " ++ prettyExpr e2 ++ ")"
+prettyExpr (If cond e1 e2) =
+  "if (" ++ prettyCondition cond ++ ") then {" ++ prettyExpr e1 ++ "} else {" ++ prettyExpr e2 ++ "}"
+prettyExpr (Call func expr) = func ++ "(" ++ prettyExpr expr ++ ")"
+prettyExpr (Dec expr) = prettyExpr expr
+
+prettyCondition :: Condition -> String
+prettyCondition (Cond comp e1 e2) =
+  prettyExpr e1 ++ " " ++ prettyComparator comp ++ " " ++ prettyExpr e2
+
+prettyComparator :: Comparator -> String
+prettyComparator Eq = "=="
+prettyComparator Lt = "<"
+prettyComparator Gt = ">"
+
+
+testProg = Program [
+  Procedure (Var "foo") [IdParam (Var "x"), IntParam (IntConst 5)] (Add (Var "x") (IntConst 1)),
+  Procedure (Var "bar") [] (If (Cond Lt (Var "a") (IntConst 10)) (IntConst 1) (IntConst 0))
+  ]
+
+-- putStrLn $ pretty testProg
 
 -- QuickCheck: all prop_* tests
 return []
