a58ae3c7d2ce6c3f34cd15db77147dd59aa75a7e
thaaoblues <thaaoblues81@gmail.com>
2025-06-02 20:12:18 +0200

    feat : function evaluation

--- MicroFP.hs
+++ MicroFP.hs
@@ -30,13 +30,13 @@ import Test.QuickCheck.All
 -- ::=
 -- ’<’ | ’==’ | ’>’
 
-data Prog = Program [Procedure]
+newtype Prog = Program [Procedure]
             deriving Show
 
 data Procedure = Procedure Expr [Param] Expr
             deriving Show
 
-data Identifier = Identifier Expr
+newtype Identifier = Identifier Expr
             deriving Show
 
 -- | Represents a parameter which can be an identifier or an integer
@@ -61,7 +61,7 @@ data Comparator = Eq | Lt | Gt deriving Show
 
 -- FP3.2
 
-fibonacci = Program [Procedure (Var "fibonacci") [IdParam (Var "n")] (If (Cond Eq (Var "n") (IntConst 0)) (Then (IntConst 1))
+fibonacci = Program [Procedure (Var "fibonacci") [IntParam (Var "n")] (If (Cond Eq (Var "n") (IntConst 0)) (Then (IntConst 0))
  (Else (If (Cond Eq (Var "n") (IntConst 1)) (Then (IntConst 1)) (Else ( Add 
     (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 1))])
     (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 2))] )
@@ -70,7 +70,7 @@ fibonacci = Program [Procedure (Var "fibonacci") [IdParam (Var "n")] (If (Cond E
     ) ) ) )]
 
 
-fib = Program [Procedure (Var "fibonacci") [IdParam (Var "n")] (If (Cond Eq (Var "n") (IntConst 0)) (Then (IntConst 1))
+fib = Program [Procedure (Var "fibonacci") [IntParam (Var "n")] (If (Cond Eq (Var "n") (IntConst 0)) (Then (IntConst 0))
  (Else (If (Cond Eq (Var "n") (IntConst 1)) (Then (IntConst 1)) (Else ( Add 
     (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 1))])
     (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 2))] )
@@ -93,7 +93,12 @@ twice =   Program [Procedure (Var "twice") [IdParam (Var "f"), IntParam (Var "x"
 
 
 
--- TODO : implement the thing with functions from functions.txt
+struct = Program [
+  Procedure (Var "add") [IntParam (Var "x"), IntParam (Var "y")] (Add (Var "x") (Var "y")),
+  Procedure (Var "inc") [IntParam (Var "x")] (Call "add" [IntParam (IntConst 1)]),
+  Procedure (Var "eleven") [] (Call "inc" [IntParam (IntConst 10)])
+  ]
+
 
 -- FP3.3
 
@@ -120,9 +125,11 @@ prettyExpr (Add e1 e2) = "(" ++ prettyExpr e1 ++ " + " ++ prettyExpr e2 ++ ")"
 prettyExpr (Sub e1 e2) = "(" ++ prettyExpr e1 ++ " - " ++ prettyExpr e2 ++ ")"
 prettyExpr (If cond e1 e2) = "if (" ++ prettyCondition cond ++ ")"
     ++ prettyExpr e1 -- then
-    ++ prettyExpr e2 -- els
-prettyExpr (Call func expr) = func ++ "(" ++ prettyExpr expr ++ ")"
-prettyExpr (Then e) = " then {\n\t" ++ prettyExpr e ++ "\n}\n"
+    ++ prettyExpr e2 -- else
+
+-- there, we need to fold as we might have multiple parameters
+prettyExpr (Call func params) = func ++ "(" ++ foldr (\x s -> prettyParam x ++ s) "" params ++ ")"
+prettyExpr (Then e) = " then {\n\t" ++ prettyExpr e ++ "\n}"
 prettyExpr (Else e) = " else {\n\t" ++ prettyExpr e ++ "\n}\n"
 
 prettyCondition :: Condition -> String
@@ -136,9 +143,54 @@ prettyComparator Gt = ">"
 
 
 testProg = Program [
-  Procedure (Var "foo") [IdParam (Var "x"), IntParam (IntConst 5)] (Add (Var "x") (IntConst 1)),
+  Procedure (Var "foo") [IntParam (Var "x"), IntParam (IntConst 5)] (Add (Var "x") (IntConst 1)),
   Procedure (Var "bar") [] (If (Cond Lt (Then (Var "a")) (Else (IntConst 10))) (IntConst 1) (IntConst 0))
   ]
+
+
+-- FP 3.4
+eval :: Prog->String->[Integer]->Integer
+eval p@(Program ((Procedure (Var fname) params fbody):fs)) function_to_call args
+    | fname == function_to_call = evalExpr fbody associated_args p
+    | otherwise = eval (Program fs) function_to_call args
+    where associated_args = zip params args
+
+
+findArgValue::[(Param,Integer)]->String->Integer
+findArgValue ((IntParam (Var argname),value):args) varname 
+    | varname == argname = value
+    | otherwise = findArgValue args varname
+
+
+evalExpr :: Expr->[(Param,Integer)]->Prog->Integer
+evalExpr (IntConst x) _ _ = x
+evalExpr (Var x) args _ = findArgValue args x -- assume local variable declaration is not possible as this is FP
+evalExpr (Mult e1 e2)args p= evalExpr e1 args p * evalExpr e2 args p
+evalExpr (Add e1 e2) args p = evalExpr e1 args p + evalExpr e2 args p
+evalExpr (Sub e1 e2) args p = evalExpr e1 args p - evalExpr e2 args p
+
+evalExpr (If cond e1 e2) args p
+  | evalCond cond args p = evalExpr e1 args p
+  | otherwise = evalExpr e2 args p
+
+evalExpr (Then e) args p = evalExpr e args p
+evalExpr (Else e) args p = evalExpr e args p
+evalExpr (Call fname params) args p = eval p fname $ evalParams params args p
+
+-- evaluate parameters to call a function from another function context
+evalParams::[Param]->[(Param,Integer)]->Prog->[Integer]
+-- only IntParam as we don't do higher order functions
+evalParams [IntParam e] args p = [evalExpr e args p]
+evalParams ((IntParam e):xs) args p = evalExpr e args p : evalParams xs args p
+
+
+evalCond :: Condition->[(Param,Integer)]->Prog->Bool
+evalCond (Cond Eq e1 e2) args p = evalExpr e1 args p == evalExpr e2 args p
+evalCond (Cond Lt e1 e2) args p = evalExpr e1 args p < evalExpr e2 args p
+evalCond (Cond Gt e1 e2) args p = evalExpr e1 args p > evalExpr e2 args p
+
+
+-- eval fibonacci "fibonacci" [5]
 
 -- putStrLn $ pretty testProg
 
