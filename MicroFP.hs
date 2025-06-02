ded9a9702bc2e71110dd5bf5f2cf3713377008d8
thaaoblues <thaaoblues81@gmail.com>
2025-06-02 17:37:45 +0200

    add : example functions ( not finished )

--- MicroFP.hs
+++ MicroFP.hs
@@ -51,8 +51,7 @@ data Expr = IntConst Integer
           | If Condition Expr Expr
           | Then Expr
           | Else Expr
-          | Dec Expr
-          | Call String Expr
+          | Call String [Param]
           deriving Show
 
 data Condition = Cond Comparator Expr Expr
@@ -60,11 +59,45 @@ data Condition = Cond Comparator Expr Expr
 
 data Comparator = Eq | Lt | Gt deriving Show
 
+-- FP3.2
+
+fibonacci = Program [Procedure (Var "fibonacci") [IdParam (Var "n")] (If (Cond Eq (Var "n") (IntConst 0)) (Then (IntConst 1))
+ (Else (If (Cond Eq (Var "n") (IntConst 1)) (Then (IntConst 1)) (Else ( Add 
+    (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 1))])
+    (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 2))] )
+    
+    )        
+    ) ) ) )]
 
 
--- Pretty-printing
+fib = Program [Procedure (Var "fibonacci") [IdParam (Var "n")] (If (Cond Eq (Var "n") (IntConst 0)) (Then (IntConst 1))
+ (Else (If (Cond Eq (Var "n") (IntConst 1)) (Then (IntConst 1)) (Else ( Add 
+    (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 1))])
+    (Call "fibonacci" [IntParam (Sub (Var "n") (IntConst 2))] )
+    
+    )        
+    ) ) ) )]
+
+sum = Program [Procedure (Var "sum") [IntParam (Var "a")] (If (Cond Eq (Var "a") (IntConst 0)) (Then (IntConst 0)) (Else (Add (Var "a") (Call "sum" [IntParam (Sub (Var "a") (IntConst 1))] ))))]
+
+div = Program [Procedure (Var "div") [IntParam (Var "x"), IntParam (Var "y")] (
+    If (Cond Lt (Var "x") (Var "y"))
+       (IntConst 0)
+       (Add (IntConst 1) (Call "div" [IntParam (Sub (Var "x") (Var "y")), IntParam (Var "y")]))
+  )]
+
+twice =   Program [Procedure (Var "twice") [IdParam (Var "f"), IntParam (Var "x")] (
+    Call "f" [IntParam (Call "f" [IntParam (Var "x")])]
+  )
+  ]
 
 
+
+-- TODO : implement the thing with functions from functions.txt
+
+-- FP3.3
+
+-- pretty-printing
 
 pretty :: Prog -> String
 pretty (Program procedures) = foldr (\x s-> x ++ "\n"++ s) "" $ map prettyProcedure procedures
@@ -85,14 +118,16 @@ prettyExpr (Var s) = s
 prettyExpr (Mult e1 e2) = "(" ++ prettyExpr e1 ++ " * " ++ prettyExpr e2 ++ ")"
 prettyExpr (Add e1 e2) = "(" ++ prettyExpr e1 ++ " + " ++ prettyExpr e2 ++ ")"
 prettyExpr (Sub e1 e2) = "(" ++ prettyExpr e1 ++ " - " ++ prettyExpr e2 ++ ")"
-prettyExpr (If cond e1 e2) =
-  "if (" ++ prettyCondition cond ++ ") then {" ++ prettyExpr e1 ++ "} else {" ++ prettyExpr e2 ++ "}"
+prettyExpr (If cond e1 e2) = "if (" ++ prettyCondition cond ++ ")"
+    ++ prettyExpr e1 -- then
+    ++ prettyExpr e2 -- els
 prettyExpr (Call func expr) = func ++ "(" ++ prettyExpr expr ++ ")"
-prettyExpr (Dec expr) = prettyExpr expr
+prettyExpr (Then e) = " then {\n\t" ++ prettyExpr e ++ "\n}\n"
+prettyExpr (Else e) = " else {\n\t" ++ prettyExpr e ++ "\n}\n"
 
 prettyCondition :: Condition -> String
-prettyCondition (Cond comp e1 e2) =
-  prettyExpr e1 ++ " " ++ prettyComparator comp ++ " " ++ prettyExpr e2
+prettyCondition (Cond cmp e1 e2) =
+  prettyExpr e1 ++ " " ++ prettyComparator cmp ++ " " ++ prettyExpr e2
 
 prettyComparator :: Comparator -> String
 prettyComparator Eq = "=="
@@ -102,7 +137,7 @@ prettyComparator Gt = ">"
 
 testProg = Program [
   Procedure (Var "foo") [IdParam (Var "x"), IntParam (IntConst 5)] (Add (Var "x") (IntConst 1)),
-  Procedure (Var "bar") [] (If (Cond Lt (Var "a") (IntConst 10)) (IntConst 1) (IntConst 0))
+  Procedure (Var "bar") [] (If (Cond Lt (Then (Var "a")) (Else (IntConst 10))) (IntConst 1) (IntConst 0))
   ]
 
 -- putStrLn $ pretty testProg
